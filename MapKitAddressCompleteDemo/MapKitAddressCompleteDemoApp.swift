550e609460671042cff4bd92025aa6666adee554
Ivan Oparin <oparin@me.com>
2025-02-06 07:40:25 +0400

    Initial Commit

---
+++ MapKitAddressCompleteDemo/MapKitAddressCompleteDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitAddressCompleteDemoApp.swift
+//  MapKitAddressCompleteDemo
+//
+//  Created by Ivan SKL on 06.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct MapKitAddressCompleteDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
